d60befc3622b9022289d34fcfd1df4deea06f3d1
Denys Nefodov <nefodovden@gmail.com>
2021-08-13 00:44:47 +0300

    add adaptive style footer and fix adaptive mobile menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
 const navBtn = document.querySelector('.nav-button');
 const mobileNav = document.querySelector('.mobile-nav')
+const body = document.body;
 
-navBtn.addEventListener('click', function () {
-    mobileNav.classList.toggle('mobile-nav-active')
-    navBtn.classList.toggle('nav-button-close')
+// клик по кнопке
+navBtn.addEventListener('click', function (e) {
+    e.stopPropagation()
+    toggleMobilenav()
 })
+
+
+// клик вне мобильной навигации
+window.addEventListener('click', function () {
+
+    if(body.classList.contains('no-scroll')) {
+        toggleMobilenav()
+    }
+})
+
+// останавливаем кли внутри открытой навигации
+
+mobileNav.addEventListener('click', function(e) {
+    e.stopPropagation();
+})
+
+// выносим одинаковые части в одну функцию и потом ее вызываем
+function toggleMobilenav() {
+    body.classList.toggle('no-scroll')
+    navBtn.classList.toggle('nav-button-close')
+    mobileNav.classList.toggle('mobile-nav-active')
+}
